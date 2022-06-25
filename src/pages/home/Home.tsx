4b001643ad3a17488948824078fb440f3179212e
github.com/HeitorBRaymundo <hbanhete@gmail.com>
2022-06-25 12:09:35 -0300

    init lola project on github

---
+++ src/pages/home/Home.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react';
+
+const Home: React.FC = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>We are Lola!</h1>
+      </header>
+    </div>
+  );
+}
+
+export default Home;
